4e1113c4d90b1b849371e770f483cc65e705fcb4
Sam Lin <greenlin111@gmail.com>
2016-11-02 18:56:16 +0800

    add api/sites create, update and delete plus rspec

--- app/controllers/api/sites_controller.rb
+++ app/controllers/api/sites_controller.rb
@@ -4,5 +4,34 @@ module Api
     def index
       @sites = Site.all
     end
+
+    def create
+    	@site= Site.new(:name => params[:name], :host => params[:host], :subdomain => params[:subdomain], :data => params[:data])
+    	if @site.save
+    		render :json => { :site => JSON.parse(@site.to_json), :message => "successfully create"}, :status => 200
+    	else
+    		render :json => {:message => "Please type correct data"}, :status => 400
+    	end
+    end
+
+    def update
+    	@site = Site.find(params[:id])
+    	if @site.update(:name => params[:name], :host => params[:host], :subdomain => params[:subdomain], :data => params[:data])
+				render :json => {:site => JSON.parse(@site.to_json) ,:message => "successfully edited!"}, :status => 200
+			else
+				render :json => { :message => "Please input the name!" }, :status => 400
+			end
+		end
+
+		def destroy
+			@site = Site.find_by_id(params[:id])
+			if @site
+				@site.destroy
+				render :json => { :message => "succesfully destroy!"}
+			else
+				render :json => { :message => "please insert right id"}, :status => 400
+			end
+		end
+
   end
 end
--- spec/controllers/api/sites_controller_spec.rb
+++ spec/controllers/api/sites_controller_spec.rb
@@ -16,4 +16,74 @@ RSpec.describe Api::SitesController, type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  describe "#create" do 
+    it "should return errors" do 
+      post "/api/sites"
+      parsed_data = JSON.parse( response.body )
+      expect(parsed_data).to eq(
+        {"message" => "Please type correct data"}
+      )
+      expect(response).to have_http_status(400)
+    end
+
+    it "should return site and success message" do 
+      post "/api/sites", :name => "foo"
+
+      expect(response).to have_http_status(200)
+      parsed_data = JSON.parse( response.body )
+      site = Site.last
+      expect(parsed_data).to eq(
+        { "site" => JSON.parse(site.to_json) ,
+          "message" => "successfully create"} 
+      )
+      expect(site.name).to eq("foo")
+    end
+  end
+
+  describe "#edit" do 
+    before { @site = Site.create(:name => "test123") }
+    it "should error if no any keys" do
+      patch "/api/sites/#{@site.id}"
+      parsed_data = JSON.parse( response.body )
+      expect(parsed_data).to eq(
+        {"message" => "Please input the name!"}
+      )
+      expect(response).to have_http_status(400)
+    end
+
+    it "should return site and success message" do 
+      patch "/api/sites/#{@site.id}", :name => "I am changing!"
+      expect(response).to have_http_status(200)
+      parsed_data = JSON.parse( response.body )
+      @site.reload
+      expect(parsed_data).to eq(
+        { "site" => JSON.parse(@site.to_json) ,
+          "message" => "successfully edited!"} 
+      )
+      expect(@site.name).to eq("I am changing!")
+    end
+  end
+
+  describe "#destroy" do 
+    before { @site = Site.create(:name => "test123") }
+    it "should return error message" do 
+      delete "/api/sites/#{@site.id+1}"
+      parsed_data = JSON.parse( response.body)
+      expect(parsed_data).to eq(
+        {"message" => "please insert right id"}
+      )
+      expect(response).to have_http_status(400)
+    end
+
+    it "should return seccess message" do 
+      delete "/api/sites/#{@site.id}"
+      parsed_data = JSON.parse( response.body)
+      expect(parsed_data).to eq(
+        {"message" => "succesfully destroy!"}
+      )
+    end
+  end
+
+
 end
