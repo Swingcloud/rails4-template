8f07b2d54ed5e692751fcd3c660e5298ddb98f43
Sam Lin <greenlin111@gmail.com>
2016-11-04 14:50:00 +0800

    add rails-i18n and modify message in api/site, add rspec

--- app/controllers/api/sites_controller.rb
+++ app/controllers/api/sites_controller.rb
@@ -4,5 +4,34 @@ module Api
     def index
       @sites = Site.all
     end
+
+
+    def create
+    	@site= Site.new(:name => params[:name], :host => params[:host], :subdomain => params[:subdomain], :data => params[:data])
+    	if @site.save
+    		render :json => { :site => JSON.parse(@site.to_json), :message => I18n.t("create.success")}, :status => 200
+    	else
+    		render :json => {:message => I18n.t("create.failure")}, :status => 400
+    	end
+    end
+
+    def update
+    	@site = Site.find(params[:id])
+    	if @site.update(:name => params[:name], :host => params[:host], :subdomain => params[:subdomain], :data => params[:data])
+				render :json => {:site => JSON.parse(@site.to_json) ,:message => I18n.t("edit.success")}, :status => 200
+			else
+				render :json => { :message => I18n.t("edit.failure") }, :status => 400
+			end
+		end
+
+		def destroy
+			@site = Site.find_by_id(params[:id])
+			if @site
+				@site.destroy
+				render :json => { :message => I18n.t("destroy.success")}
+			else
+				render :json => { :message => I18n.t("destroy.failure")}, :status => 400
+			end
+		end
   end
 end
--- spec/controllers/api/sites_controller_spec.rb
+++ spec/controllers/api/sites_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Api::SitesController, type: :request do
+
   describe "#index" do
     before { FactoryGirl.create(:site) }
-
+ 
     subject do
-      get "/api/sites.json"
+      get "/api/sites.json", {},  'HTTP_ACCEPT_LANGUAGE' => 'zh-TW' 
       JSON.parse(response.body)
     end
 
@@ -16,4 +17,73 @@ RSpec.describe Api::SitesController, type: :request do
       expect(response.status).to eq(200)
     end
   end
+
+  describe "#create" do 
+    it "should return errors" do 
+      post "/api/sites", {},  'HTTP_ACCEPT_LANGUAGE' => 'zh-TW'
+      parsed_data = JSON.parse( response.body )
+      expect(parsed_data).to eq(
+        {"message" => I18n.t("create.failure")}
+      )
+      expect(response).to have_http_status(400)
+    end
+
+    it "should return site and success message" do 
+      post "/api/sites", {:name => "foo"}, {'HTTP_ACCEPT_LANGUAGE' => 'zh-TW'}
+
+      expect(response).to have_http_status(200)
+      parsed_data = JSON.parse( response.body )
+      site = Site.last
+      expect(parsed_data).to eq(
+        { "site" => JSON.parse(site.to_json) ,
+          "message" => I18n.t("create.success")} 
+      )
+      expect(site.name).to eq("foo")
+    end
+  end
+
+  describe "#edit" do 
+    before { @site = Site.create(:name => "test123") }
+    it "should error if no any keys" do
+      patch "/api/sites/#{@site.id}", {},  'HTTP_ACCEPT_LANGUAGE' => 'zh-TW'
+      parsed_data = JSON.parse( response.body )
+      expect(parsed_data).to eq(
+        {"message" => I18n.t("edit.failure")}
+      )
+      expect(response).to have_http_status(400)
+    end
+
+    it "should return site and success message" do 
+      patch "/api/sites/#{@site.id}", {:name => "I am changing!"} , {'HTTP_ACCEPT_LANGUAGE' => 'zh-TW'}
+      expect(response).to have_http_status(200)
+      parsed_data = JSON.parse( response.body )
+      @site.reload
+      expect(parsed_data).to eq(
+        { "site" => JSON.parse(@site.to_json) ,
+          "message" => I18n.t("edit.success")} 
+      )
+      expect(@site.name).to eq("I am changing!")
+    end
+  end
+
+  describe "#destroy" do 
+    before { @site = Site.create(:name => "test123") }
+    it "should return error message" do 
+      delete "/api/sites/#{@site.id+1}", {},  'HTTP_ACCEPT_LANGUAGE' => 'zh-TW'
+      parsed_data = JSON.parse( response.body)
+      expect(parsed_data).to eq(
+        {"message" => I18n.t("destroy.failure")}
+      )
+      expect(response).to have_http_status(400)
+    end
+
+    it "should return seccess message" do 
+      delete "/api/sites/#{@site.id}", {},  'HTTP_ACCEPT_LANGUAGE' => 'zh-TW'
+      parsed_data = JSON.parse( response.body)
+      expect(parsed_data).to eq(
+        {"message" => I18n.t("destroy.success")}
+      )
+    end
+  end
+
 end
